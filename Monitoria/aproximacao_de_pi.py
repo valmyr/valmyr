b0cbdfb778083440066dc7a40f0b516b0b25dd00
valmyr <65004196+valmyr@users.noreply.github.com>
2020-10-09 10:22:42 -0300

    Update aproximacao_de_pi.py

--- Monitoria/aproximacao_de_pi.py
+++ Monitoria/aproximacao_de_pi.py
@@ -10,19 +10,20 @@ O valor de n: quantidade de termos
 SAÍDA
 
 O valor aproximado de π com 5 casas decimais.'''
-n = int(input())
-num = 2
-den = 1
-pi = 1
+N = int(input())
+n = 2
+d=1
 i = 0
-while(i < n):
-    j = 0
-    while(j < 2):
-        pi *= float(num/(den))
-        i+=1
-        if(j == 0):
-            den+=2
-        else:
-            num+=2
+j = 0
+pi =1
+while(i < 10):
+    pi*=n/d
+    if(j < 1):
         j+=1
-print("{:.5f}".format(2*pi))
+        d+=2
+    else:
+        j = 0
+        n += 2
+    i+=1
+
+print(pi*2)
