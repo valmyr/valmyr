624b433a91ce27b7e5b4d7aca8878492abec9655
valmyr <65004196+valmyr@users.noreply.github.com>
2021-01-22 23:57:09 -0300

    Update uri1212.py

--- uri1212.py
+++ uri1212.py
@@ -40,3 +40,10 @@ while(True):
               else:
                      print(f"{carry} carry operations.",end="\n")
                      
+'''As crianças são ensinadas a adicionar vários dígitos da direita para a esquerda, um dígito de cada vez. Muitos acham a operação "vai 1" (em inglês chamada de "carry", na qual o valor 1 é carregado de uma posição para ser adicionado ao dígito seguinte) um desafio significativo. Seu trabalho é para contar o número de operações de carry para cada um dos problemas de adição apresentados para que os educadores possam avaliar a sua dificuldade.
+
+Entrada
+Cada linha de entrada contém dois inteiros sem sinal com no máximo 9 dígitos. A última linha de entrada contém 0 0.
+
+Saída
+Para cada linha de entrada, com exceção da última, você deve computar e imprimir a quantidade de operações "leva 1" que resultam da adição dos 2 números, no formato apresentado no exemplo abaixo.'''
