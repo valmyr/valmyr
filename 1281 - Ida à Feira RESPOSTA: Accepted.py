c29193d0f14bf5feead6fc015d93a1570a233663
valmyr <65004196+valmyr@users.noreply.github.com>
2021-02-04 17:15:40 -0300

    Update 1281 - Ida à Feira RESPOSTA: Accepted.py

--- 1281 - Ida à Feira RESPOSTA: Accepted.py
+++ 1281 - Ida à Feira RESPOSTA: Accepted.py
@@ -11,5 +11,5 @@ for i in range(limite):
     for i in range(quantidadeDeProdutosDiferentes):
         produto,quantidadeDesejada = input().split()
         valorTotal_A_Pagar+=dicionarioComOsProdutosDisponiveisE_OsPrecos[produto]*float(quantidadeDesejada)
-    print(f'R$ {valorTotal_A_Pagar}',end='\n')
+    print(f'R$ {valorTotal_A_Pagar:.2f}',end='\n')
     dicionarioComOsProdutosDisponiveisE_OsPrecos.clear()
