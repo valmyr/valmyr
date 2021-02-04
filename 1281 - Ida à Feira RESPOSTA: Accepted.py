090cc45c1706f9e8e58c60cf2441a7c223327b2a
valmyr <65004196+valmyr@users.noreply.github.com>
2021-02-04 17:14:37 -0300

    Create  1281 - Ida à Feira RESPOSTA: Accepted.py

---
+++ 1281 - Ida à Feira RESPOSTA: Accepted.py
@@ -0,0 +1,15 @@
+limite = int(input())
+for i in range(limite):
+    quantidadeDeProdutosDisponiveis = int(input())
+    dicionarioComOsProdutosDisponiveisE_OsPrecos = {}
+    for j in range(quantidadeDeProdutosDisponiveis):
+        produto,precoPorQuilo = input().split()
+        precoPorQuilo = float(precoPorQuilo)
+        dicionarioComOsProdutosDisponiveisE_OsPrecos[produto] = precoPorQuilo
+    quantidadeDeProdutosDiferentes = int(input())
+    valorTotal_A_Pagar = 0
+    for i in range(quantidadeDeProdutosDiferentes):
+        produto,quantidadeDesejada = input().split()
+        valorTotal_A_Pagar+=dicionarioComOsProdutosDisponiveisE_OsPrecos[produto]*float(quantidadeDesejada)
+    print(f'R$ {valorTotal_A_Pagar}',end='\n')
+    dicionarioComOsProdutosDisponiveisE_OsPrecos.clear()
