6e52d83a4a291e5b4fde127b40fac0998f2ccfe7
valmyr <65004196+valmyr@users.noreply.github.com>
2021-02-03 22:23:54 -0300

    Create 1171 Frequência de Números.py

---
+++ 1171 Frequência de Números.py
@@ -0,0 +1,13 @@
+d = {}
+limite = int(input())
+for i in range(limite):
+              s = int(input())
+              try:
+
+                     d[s]+=1
+              except KeyError:
+                     d[s]=1
+d = dict(sorted(d.items(),key=lambda x: x[0]))
+for i in d:
+       print(f'{i} aparece {d[i]} vez(es)',end='\n')
+
