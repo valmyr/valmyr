2b34349e954964a09e183fa8d6e5e8094cf3c3a4
valmyr <65004196+valmyr@users.noreply.github.com>
2021-02-03 22:32:54 -0300

    Update 1171 Frequência de Números.py

--- 1171 Frequência de Números.py
+++ 1171 Frequência de Números.py
@@ -3,7 +3,6 @@ limite = int(input())
 for i in range(limite):
               s = int(input())
               try:
-
                      d[s]+=1
               except KeyError:
                      d[s]=1
