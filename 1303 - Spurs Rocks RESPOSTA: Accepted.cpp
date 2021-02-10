f92f3c8caf0b2181f6cac384e709e5e5156010df
valmyr <65004196+valmyr@users.noreply.github.com>
2021-02-10 14:10:51 -0300

    Create 1303 - Spurs Rocks RESPOSTA: Accepted.cpp

---
+++ 1303 - Spurs Rocks RESPOSTA: Accepted.cpp
@@ -0,0 +1,120 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+class SpursRocks{
+    private:
+        int inscricaoNaLiga;
+        int marcados;
+        int recebidos;
+        int pontos;
+    public:
+        SpursRocks(int = 0,int = 0, int = 0, int =0);
+        void setPontos(int);
+        void setInscricaoNaLiga(int);
+        void setMarcados(int);
+        void setRecebidos(int);
+        int getInscricaoNaLiga()const;
+        int getPontos()const;
+        int getMarcados()const;
+        int getRecebidos()const;
+};
+SpursRocks::SpursRocks(int inscricaoNaLiga,int pontos,int marcados, int recebidos){
+    this->inscricaoNaLiga = inscricaoNaLiga;
+    this->pontos = pontos;
+    this->marcados = 0;
+    this->recebidos = 0;
+}
+void SpursRocks::setMarcados(int marcados){
+    this->marcados+=marcados;
+}
+void SpursRocks::setRecebidos(int recebidos){
+    this->recebidos+=recebidos;
+}
+int SpursRocks::getInscricaoNaLiga()const{
+    return this->inscricaoNaLiga;
+}
+int SpursRocks::getPontos()const{
+    return this->pontos;
+}
+void SpursRocks::setPontos(int pontos){
+    this->pontos = pontos;
+}
+void SpursRocks::setInscricaoNaLiga(int inscricaoNaLiga){
+    this->inscricaoNaLiga = inscricaoNaLiga;
+}
+int SpursRocks::getMarcados()const{
+    return this->marcados;
+}
+int SpursRocks::getRecebidos()const{
+    return this->recebidos;
+}
+bool customOrd(SpursRocks c1,SpursRocks c2){
+    //cout<<c1.getInscricaoNaLiga()<<" inscricaoNaLiga "<<c2.getInscricaoNaLiga()<<endl;
+    //cout<<c1.getPontos()<<" Pontos "<<c2.getPontos()<<endl;
+    //cout<<c1.getMarcados()<<" Pontos "<<c2.getMarcados()<<endl;
+    //cout<<c1.getRecebidos()<<" recebidos "<<c2.getRecebidos()<<endl;
+    //cout<<float(c1.getMarcados())/float(c1.getRecebidos())<<" avange "<<float(c2.getMarcados())/float(c2.getRecebidos())<<endl;
+    if(c1.getPontos() == c2.getPontos()){
+        if(float(c1.getMarcados())/float(c1.getRecebidos()) == float(c2.getMarcados())/float(c2.getRecebidos())){
+            if(c1.getMarcados() == c2.getMarcados()){
+    //            cout<<"Nivel 4"<<endl;
+                return c1.getInscricaoNaLiga() < c2.getInscricaoNaLiga();
+            }
+    //        cout<<"Nivel 3"<<endl;
+            return c1.getMarcados() < c2.getMarcados();
+        }
+    //    cout<<"Nivel 2"<<endl;
+        return float(c1.getMarcados())/float(c1.getRecebidos()) > float(c2.getMarcados())/float(c2.getRecebidos());
+    }
+    //cout<<"Nivel 1"<<endl;
+    return c1.getPontos() > c2.getPontos();
+}
+void classificador(vector<SpursRocks>classif){
+    sort(classif.begin(),classif.end(),customOrd);
+    for(int i = 0; i < classif.size()-1; i++)
+        cout<<classif[i].getInscricaoNaLiga()<<" ";
+    cout<<classif[classif.size()-1].getInscricaoNaLiga()<<endl;
+}
+
+int main(){
+    int n, numeroDeInscricaoNaLiga, numeroDePontos;
+    int contador = 1;
+    while(true){
+        cin>>n;
+        if(n == 0)
+            break;
+        if(contador > 1)
+            cout<<endl;
+        int limite = n*(n-1)/2;
+        {
+            vector<SpursRocks>classif(n);
+            for(int i = 0; i < limite; i++){
+                cin>>numeroDeInscricaoNaLiga;
+                cin>>numeroDePontos;
+                SpursRocks tmp1(numeroDeInscricaoNaLiga,numeroDePontos);
+                cin>>numeroDeInscricaoNaLiga;
+                cin>>numeroDePontos;
+                SpursRocks tmp2(numeroDeInscricaoNaLiga,numeroDePontos);
+                classif[tmp1.getInscricaoNaLiga()-1].setMarcados(tmp1.getPontos());
+                classif[tmp1.getInscricaoNaLiga()-1].setRecebidos(tmp2.getPontos());
+                classif[tmp2.getInscricaoNaLiga()-1].setMarcados(tmp2.getPontos());
+                classif[tmp2.getInscricaoNaLiga()-1].setRecebidos(tmp1.getPontos());
+                if (tmp1.getPontos() > tmp2.getPontos()) {
+                    classif[tmp1.getInscricaoNaLiga()-1].setPontos(classif[tmp1.getInscricaoNaLiga()-1].getPontos()+2);
+                    classif[tmp2.getInscricaoNaLiga()-1].setPontos(classif[tmp2.getInscricaoNaLiga()-1].getPontos()+1);
+                    classif[tmp1.getInscricaoNaLiga()-1].setInscricaoNaLiga(tmp1.getInscricaoNaLiga());                                classif[tmp2.getInscricaoNaLiga()-1].setInscricaoNaLiga(tmp2.getInscricaoNaLiga());
+                }else{
+                    classif[tmp1.getInscricaoNaLiga()-1].setPontos(classif[tmp1.getInscricaoNaLiga()-1].getPontos()+1);
+                    classif[tmp2.getInscricaoNaLiga()-1].setPontos(classif[tmp2.getInscricaoNaLiga()-1].getPontos()+2);
+                    classif[tmp1.getInscricaoNaLiga()-1].setInscricaoNaLiga(tmp1.getInscricaoNaLiga());
+                    classif[tmp2.getInscricaoNaLiga()-1].setInscricaoNaLiga(tmp2.getInscricaoNaLiga());
+                }
+            }
+            cout<<"Instancia "<<(contador++)<<endl;
+            classificador(classif);
+            classif.clear();
+        }
+    }
+    return 0;
+}
