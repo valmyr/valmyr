ff3e7dc11b1e07de6a0520da66d5d34c364fc0e4
valmyr <65004196+valmyr@users.noreply.github.com>
2021-02-14 00:47:46 -0300

    Create  1800 - Onde Estão Minhas Chaves RESPOSTA: Accepted.py

---
+++ 1800 - Onde Estão Minhas Chaves RESPOSTA: Accepted.py
@@ -0,0 +1,10 @@
+escritoriosNaUltimaSemana,escritoriosNosUltimosDias = map(int,input().split())
+escritorios = list(map(int,input().split()))
+for i in range(q):
+    escritorio = int(input())
+    if(escritorio in escritorios):
+        print("0")
+    else:
+        print("1")
+        escritorios.append(qInteiros)
+    print(end='\n')
